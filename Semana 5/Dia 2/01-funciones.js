58dc763bc41a2c3ba1a866ce0e8f9f65706f9910
Pedro Ancajima <a.ancajimaa@pucp.edu.pe>
2022-03-31 23:05:08 -0500

    dia 2 semana 5

---
+++ Semana 5/Dia 2/01-funciones.js
@@ -0,0 +1,8 @@
+function calcularAreaTriangulo(base, altura){
+    let area = (base*altura)/2
+    return area
+}
+let resultado = calcularAreaTriangulo(10,15)
+console.log(resultado)
+let resultado2 = calcularAreaTriangulo(100,50)
+console.log(resultado2)
